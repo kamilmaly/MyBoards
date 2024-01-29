0a916936528de953c768f100650bae25aa873d6a
kamilmaly <k.malysa94@gmail.com>
2024-01-29 15:39:16 +0100

    Adding DbContext and MsSql connection configuration

---
+++ Entities/MyBoardsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
+        { 
+        
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyBoardsContext>(
+        option=> option.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsConnectionString"))
+    );
 
 var app = builder.Build();
 
