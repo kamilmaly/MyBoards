851ab741cfb41d2b430b280b2662d4a22ae38564
kamilmaly <k.malysa94@gmail.com>
2024-01-30 22:44:52 +0100

    Model configuration - fluent API

--- Entities/MyBoardsContext.cs
+++ Entities/MyBoardsContext.cs
@@ -5,8 +5,8 @@ namespace MyBoards.Entities
     public class MyBoardsContext : DbContext
     {
         public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
-        { 
-        
+        {
+
         }
         public DbSet<WorkItem> WorkItems { get; set; }
         public DbSet<User> Users { get; set; }
@@ -17,5 +17,19 @@ namespace MyBoards.Entities
 
         public DbSet<Address> Addresses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.State).IsRequired();
+                eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+                eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.RemaningWork).HasPrecision(14, 2);
+            });
+
+        }
     }
 }
--- Entities/WorkItem.cs
+++ Entities/WorkItem.cs
@@ -1,24 +1,27 @@
-﻿namespace MyBoards.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBoards.Entities
 {
     public class WorkItem
     {
         public int Id { get; set; }
         public string State { get; set; }
-
         public string Area { get; set; }
 
         public string IterationPath { get; set; }
 
         public int Priority { get; set; }
         // Epic
-        public DateTime? StartDate { get; set; }    
+        public DateTime? StartDate { get; set; }
+
         public DateTime? EndDate { get; set;}
 
         //Issue
         public decimal Efford { get; set; }
 
         public string Activity { get; set; }
-
         public decimal RemaningWork { get; set; }
 
         public string Type { get; set; }
