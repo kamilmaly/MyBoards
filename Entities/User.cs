3eb753f10259a3291f356317f284ceba551c884b
kamilmaly <k.malysa94@gmail.com>
2024-02-07 21:26:48 +0100

    Applying automatic migration

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,9 +3,7 @@
     public class User
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
+        public  string FullName { get; set; }
 
         public string Email { get; set; }
 
---
+++ Migrations/20240207193117_FullName.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class FullName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+              name: "FullName",
+              table: "Users",
+              type: "nvarchar(max)",
+              nullable: true);
+
+            migrationBuilder.Sql(@"
+                UPDATE Users
+                SET FullName = FirstName + ' ' + LastName"
+                );
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+               name: "LastName",
+               table: "Users");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+            name: "LastName",
+            table: "Users",
+            type: "nvarchar(max)",
+            nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.DropColumn(
+              name: "FullName",
+              table: "Users");
+        }
+    }
+}
